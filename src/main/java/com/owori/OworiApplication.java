43f9d43fbf1fdf7e5cf34bac22567f4c7b246608
김승진 <ohksj77@gmail.com>
2023-07-02 17:43:22 +0900

    feat: backend project init

---
+++ src/main/java/com/owori/OworiApplication.java
@@ -0,0 +1,13 @@
+package com.owori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OworiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OworiApplication.class, args);
+	}
+
+}
