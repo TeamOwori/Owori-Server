5dd12e58f72485ddb45e36c8606c1b1730fa2b5d
김승진 <ohksj77@gmail.com>
2023-07-12 16:00:51 +0900

    chore: webclient 예외 처리

--- src/main/java/com/owori/domain/member/exception/WebClientException.java
+++ src/main/java/com/owori/domain/member/exception/WebClientException.java
@@ -1,7 +1,7 @@
 package com.owori.domain.member.exception;
 
 public class WebClientException extends IllegalStateException {
-    private static final String MESSAGE = "";
+    private static final String MESSAGE = "카카오의 요청을 받을 수 없습니다.";
 
     public WebClientException() {
         super(MESSAGE);
