5101c16a4993841764cb6e1bb1436301d227404b
zer0silver <duddms0_0@naver.com>
2023-07-22 13:37:39 +0900

    feat: methodArgumentNotValidException 추가

--- src/main/java/com/owori/global/advice/GlobalErrorAdvice.java
+++ src/main/java/com/owori/global/advice/GlobalErrorAdvice.java
@@ -4,6 +4,7 @@ import com.owori.global.exception.EntityNotFoundException;
 import com.owori.global.exception.NoAuthorityException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -23,6 +24,11 @@ public class GlobalErrorAdvice {
 
     @ExceptionHandler(NoAuthorityException.class)
     public ResponseEntity<ErrorResponse> noAuthorityException(NoAuthorityException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(e.getMessage()));
     }
 }
