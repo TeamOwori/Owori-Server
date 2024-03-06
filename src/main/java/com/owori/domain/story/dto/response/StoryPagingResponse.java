30355695261bcad99d7eb5aaa13e6197f63ff7c0
zer0silver <duddms0_0@naver.com>
2024-03-06 22:01:31 +0900

    refactor: dto 생성 방식 변경 (mapper 이용)

--- src/main/java/com/owori/domain/story/dto/response/StoryPagingResponse.java
+++ src/main/java/com/owori/domain/story/dto/response/StoryPagingResponse.java
@@ -1,30 +1,19 @@
 package com.owori.domain.story.dto.response;
 
-import com.owori.domain.story.entity.Story;
-import com.owori.domain.story.mapper.StoryMapper;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class StoryPagingResponse {
-    private static final StoryMapper mapper = new StoryMapper();
     private List<FindAllStoryResponse> stories = new ArrayList<>();
     private boolean hasNext;
-
-    public static StoryPagingResponse of(Page<Story> pageStory){
-        return new StoryPagingResponse(getContents(pageStory.getContent()), pageStory.hasNext());
-    }
-
-    private static List<FindAllStoryResponse> getContents(List<Story> stories){
-        return stories.stream().map(story -> mapper.toFindAllStoryResponse(story)).toList();
-    }
-
 }
